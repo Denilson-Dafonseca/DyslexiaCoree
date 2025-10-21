af562e0e53378f4dfba07cc7a0189d0683b92150
Denilson Dafonseca <denilkson.dafonseca99@gmail.com>
2025-10-21 02:43:01 +0200

    Initial Commit

--- Dyslexia/settings.py
+++ Dyslexia/settings.py
@@ -3,35 +3,31 @@ import os
 import dj_database_url
 from dotenv import load_dotenv
 
-# -----------------------------
-# Load environment variables
-# -----------------------------
-load_dotenv()
 
-# -----------------------------
-# Paths
-# -----------------------------
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# -----------------------------
-# Security
-# -----------------------------
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-temp-key-for-dev-only')
 
+#load our environment 
+load_dotenv()
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-9+_@mp%sklldf6l7lq0o*1=h=y!y$@mb#n1#v(i4sptl-aoysm'
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "dyslexiacore-production.up.railway.app", "https://dyslexiacore-production.up.railway.app"
-    
-]
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = []
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://dyslexiacore-production.up.railway.app",
-]
 
-# -----------------------------
-# Applications
-# -----------------------------
+
+
+# Application definition
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -56,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    
+   
 ]
 
 ROOT_URLCONF = 'Dyslexia.urls'
@@ -78,9 +76,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Dyslexia.wsgi.application'
 
-# -----------------------------
+
 # Database
-# -----------------------------
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -94,42 +94,59 @@ DATABASES = {
 
 DATABASES["default"] = dj_database_url.parse('postgresql://postgres:KEKHvVyQKupUEBzWNqsFmQBFBmbNOetV@interchange.proxy.rlwy.net:44571/railway')
 
-# -----------------------------
+
+
 # Password validation
-# -----------------------------
+# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
 
-# -----------------------------
+
 # Internationalization
-# -----------------------------
+# https://docs.djangoproject.com/en/5.2/topics/i18n/
+
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'UTC'
+
 USE_I18N = True
+
 USE_TZ = True
 
-# -----------------------------
-# Static and media files
-# -----------------------------
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'Dyslexia_static_files']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Dyslexia_static_files')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'Dyslexiafile')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
-# -----------------------------
+MEDIA_ROOT = os.path.join(BASE_DIR, 'site')
+MEDIA_URL = '/site/'
+
 # Default primary key field type
-# -----------------------------
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# -----------------------------
-# PayPal Settings
-# -----------------------------
-PAYPAL_TEST = False  # Ensure this is False for live payments
+#PAYPAL INFO
+
+PAYPAL_TEST = False  # MUST TURN TO FALSE
 PAYPAL_RECEIVER_EMAIL = os.environ.get('email')

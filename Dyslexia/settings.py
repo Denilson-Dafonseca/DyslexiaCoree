8aa99c10724317521caab965bb7f3c6bb7d4d0ef
Denilson Dafonseca <denilkson.dafonseca99@gmail.com>
2025-10-24 04:45:46 +0200

    correc

--- Dyslexia/settings.py
+++ Dyslexia/settings.py
@@ -130,15 +130,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Dyslexia_static_files')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'Dyslexiafile')
+# --- Static Files ---
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'Dyslexia_static_files']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'site')
+# --- Media Files ---
 MEDIA_URL = '/site/'
+MEDIA_ROOT = BASE_DIR / 'site'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

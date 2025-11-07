3c5b9ed503b49291e473f0aec394d34f721805b7
Denilson Dafonseca <denilkson.dafonseca99@gmail.com>
2025-11-07 17:09:01 +0200

    run the damn thing

--- Dyslexia/urls.py
+++ Dyslexia/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
+from . import settings
 from django.conf.urls.static import static 
 
 
@@ -9,9 +9,8 @@ urlpatterns = [
     path('', include('casa.urls')),
     path('cart/', include('cart.urls')),
     path('pay/', include('pay.urls')),
- ]
+    
+ ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 

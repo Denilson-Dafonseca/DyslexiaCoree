55a050702e5cfc881f7d3c580171ff56319c7f4a
Denilson Dafonseca <denilkson.dafonseca99@gmail.com>
2025-11-07 16:06:22 +0200

    fix csrf

--- Dyslexia/settings.py
+++ Dyslexia/settings.py
@@ -20,10 +20,8 @@ SECRET_KEY = 'django-insecure-9+_@mp%sklldf6l7lq0o*1=h=y!y$@mb#n1#v(i4sptl-aoysm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
-
-
+ALLOWED_HOSTS = ['https://dyslexiacore.xyz','dyslexiacore.xyz', 'dyslexiacoree-production.up.railway.app', 'https://dyslexiacoree-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://dyslexiacore.xyz','https://dyslexiacoree-production.up.railway.app']
 
 # Application definition
 

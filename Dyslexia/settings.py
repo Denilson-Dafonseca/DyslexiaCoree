77b9959062da62d3f71d662511e8b7481427a503
Denilson Dafonseca <denilkson.dafonseca99@gmail.com>
2026-04-11 01:12:55 +0200

    Do it

--- Dyslexia/settings.py
+++ Dyslexia/settings.py
@@ -84,10 +84,12 @@ WSGI_APPLICATION = 'Dyslexia.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    "default": dj_database_url.config(
+        default="postgresql://postgres:KEKHvVyQKupUEBzWNqsFmQBFBmbNOetV@interchange.proxy.rlwy.net:44571/railway",
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
-
-DATABASES["default"] = dj_database_url.parse('postgresql://postgres:KEKHvVyQKupUEBzWNqsFmQBFBmbNOetV@interchange.proxy.rlwy.net:44571/railway')
-
 
 
 # Password validation

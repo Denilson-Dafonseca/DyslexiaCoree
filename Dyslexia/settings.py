0cf4526d0fc8aeb5c31ba5321f3cbf45a3bb9f7b
Denilson Dafonseca <denilkson.dafonseca99@gmail.com>
2026-04-11 14:50:59 +0200

    fast

--- Dyslexia/settings.py
+++ Dyslexia/settings.py
@@ -146,12 +146,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_TIMEOUT = 5
 
 EMAIL_HOST_USER = os.environ.get("BREVO_LOGIN")
 EMAIL_HOST_PASSWORD = os.environ.get("BREVO_KEY")
-
-

6a06d6703105e84bd608422571bef0888f5362c8
Denilson Dafonseca <denilkson.dafonseca99@gmail.com>
2026-06-10 16:19:49 +0200

    link

--- Dyslexia/settings.py
+++ Dyslexia/settings.py
@@ -22,13 +22,11 @@ SECRET_KEY = 'django-insecure-9+_@mp%sklldf6l7lq0o*1=h=y!y$@mb#n1#v(i4sptl-aoysm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dyslexiacore.xyz','.vercel.app']
+ALLOWED_HOSTS = ['dyslexia-coree.vercel.app']
 
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://dyslexiacore.xyz',
-    'https://www.dyslexiacore.xyz',
-    'https://vercel.app',
+    'https://dyslexia-coree.vercel.app'
 ]
 
 

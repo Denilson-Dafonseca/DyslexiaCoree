7538bce86dae3a4a94dcfc8b0a03f7bf59c63621
Denilson Dafonseca <denilkson.dafonseca99@gmail.com>
2026-06-15 15:43:45 +0200

    work more

--- Dyslexia/settings.py
+++ Dyslexia/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9+_@mp%sklldf6l7lq0o*1=h=y!y$@mb#n1#v(i4sptl-aoysm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".vercel.app",'dyslexia-coree.vercel.app', 'dyslexiacore.xyz']
 

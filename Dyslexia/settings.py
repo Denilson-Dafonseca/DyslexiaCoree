b4ba71bef46bd3ba3c4b30d5fdb961a0a6f4b396
Denilson Dafonseca <denilkson.dafonseca99@gmail.com>
2026-06-15 15:00:10 +0200

    media

--- Dyslexia/settings.py
+++ Dyslexia/settings.py
@@ -122,8 +122,21 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # -------------------------
 # MEDIA FILES
 # -------------------------
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+INSTALLED_APPS = [
+    ...
+    "cloudinary",
+    "cloudinary_storage",
+]
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
 
 # Maximum size allowed for file uploads 
 DATA_UPLOAD_MAX_MEMORY_SIZE = 524288000 
